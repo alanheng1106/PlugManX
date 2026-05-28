2708e7b6960358b3133a63a710ae5a731205b801
HaeImDuck <yizheng5946@icloud.com>
2026-05-28 14:22:25 +0800

    Bump project version to 3.0.6

--- plugman-velocity/src/main/java/velocity/com/rylinaux/plugman/PlugManVelocity.java
+++ plugman-velocity/src/main/java/velocity/com/rylinaux/plugman/PlugManVelocity.java
@@ -52,7 +52,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "plugmanvelocity",
         name = "PlugManVelocity",
-        version = "3.0.5",
+        version = "3.0.6",
         description = "Plugin manager for Velocity servers.",
         authors = {"rylinaux", "TestAccount666"}
 )
